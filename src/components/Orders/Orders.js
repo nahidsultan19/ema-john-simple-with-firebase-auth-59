e2c2397a4f7e8c032395316d40231692963e79d0
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-31 02:28:53 +0600

    added useCart custom hooks

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
-            <h2>This is orders page</h2>
+            <h2>This is orders page:{products.length}</h2>
+            <p>cart has: {cart.length}</p>
         </div>
     );
 };
