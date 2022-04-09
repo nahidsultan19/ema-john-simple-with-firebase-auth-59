7c14642a8b0f3227aaf74d81911197859e97bc5a
nahidsultan19 <nahidsultan38@gmail.com>
2022-04-09 23:23:10 +0600

    added handler for input field and form submit.

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -2,7 +2,9 @@
  * 1. create a new project in console.google.com
  * 2. npm install firebase.
  * 3. create firebase.init.js and import getAuth to export auth
- * 4. 
+ * 4. firebase setting > authetication>enable email and password auth
+ * 5. Create Login, SignUp compnent,setup route
+ * 6. attach form field handler and form submit handler
 */
 
 
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleConfirmPassword = event => {
+        setConfirmPassword(event.target.value)
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault()
+        if (password !== confirmPassword) {
+            setError('Password did not match');
+            return;
+        }
+    }
+
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form action="" onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name='email' placeholder='Your Email' required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name='password' placeholder='Your Password' required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPassword} type="password" name='confirm-password' placeholder='Your Confirm Password' required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>Already have an account? <Link className='form-link' to='/login'>Login</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
